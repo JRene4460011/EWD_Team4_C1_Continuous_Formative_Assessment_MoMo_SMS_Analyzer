f35de36523b258c5398db38c3076e923ec6b07a4
JRene4460011 <j.nsengiyum@alustudent.com>
2026-09-15 20:52:58 +0200

    Add transactions table SQL script

--- database/database_setup.sql
+++ database/database_setup.sql
@@ -5,6 +5,21 @@
 
 
 -- SQL Codes for the Transactions Table
+CREATE TABLE IF NOT EXISTS Transactions (
+    Transaction_id INT PRIMARY KEY AUTO_INCREMENT,
+    Sender_user_id INT NOT NULL,
+    Recipient_user_id INT NOT NULL,
+    Category_id INT NOT NULL,
+    Amount DECIMAL(10, 2) NOT NULL,
+    fee DECIMAL(10, 2) NOT NULL,
+    Transaction_datetime DATETIME NOT NULL,
+    Balance_after DECIMAL(10, 2) NOT NULL,
+    Status ENUM('Pending', 'Completed', 'Failed') NOT NULL,
+    External_transaction_id VARCHAR(255),
+    FOREIGN KEY (Sender_user_id) REFERENCES Users(User_id),
+    FOREIGN KEY (Recipient_user_id) REFERENCES Users(User_id),
+    FOREIGN KEY (Category_id) REFERENCES TransactionCategories(Category_id)
+);
 
 
 -- SQL Codes for the SystemLogs Table
